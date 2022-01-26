ef53c6398157ee9183fad7e6c0c44a5e45b00707
Michael.T <michael.truong@student.rjuhsd.us>
2022-01-26 09:31:02 -0800

    asked for user bday info

--- birthday.py
+++ birthday.py
@@ -1,1 +1,8 @@
+import datetime
+
 print("welcome")
+print("Happy Birthday")
+m = input("Enter your birth month: >")
+d = input("Enter your birth day: >")
+y = input("Enter your birth year: >")
+print("you were born on:", m, d, y)
